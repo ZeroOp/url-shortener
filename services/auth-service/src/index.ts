2435cfd9bdb3665a92ded505519d522df240f69a
Zeroop <jayaramnaik725@gmail.com>
2026-03-25 00:10:48 +0530

    Setup mongoDB and replicas and created auth-deployment

--- services/auth-service/src/index.ts
+++ services/auth-service/src/index.ts
@@ -1,6 +1,24 @@
+import mongoose from "mongoose";
 import { app } from "./app";
 
+const start = async () => {
+    if (!process.env.JWT_KEY) {
+        throw new Error("JWT must be defined");
+    }
+    if(!process.env.MONGO_URI) {
+        throw new Error("MONGO_URI must be defined");
+    }
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
-})
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Connected to MongoDB");
+    }
+    catch (err) {
+        console.log(err);
+    }
+    app.listen(3000, () => {
+        console.log("Auth service is running on port 3000");
+    })
+}
+
+start();
