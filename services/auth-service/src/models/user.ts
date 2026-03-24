3413ab99ccfa7d7c72c0d7606db710235d73f7c9
Zeroop <jayaramnaik725@gmail.com>
2026-03-24 21:19:31 +0530

    Added user schema and password hashing before saving user to the DB

---
+++ services/auth-service/src/models/user.ts
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+import { Password } from "../utils/password";
+
+interface UserAttrs {
+    email: string; 
+    password: string;
+}
+
+interface UserDoc extends mongoose.Document {
+    id: string;
+    email: string;
+    password: string;
+}
+
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: UserAttrs): UserDoc;
+}
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,  
+        required: true
+    },
+    password: {
+        type: String, 
+        required: true
+    }
+}, {
+    toJSON : {
+        transform(doc, ret: any) {
+            ret.id = ret._id;
+            delete ret._id;
+            delete ret.password;
+            delete ret.__v;
+        }
+    }
+});
+// Before saving userSchema check weather password have been changed or not. If changed then hash the password before saving it to the DB
+userSchema.pre('save', async function() {
+    if (this.isModified('password')) {
+        const hashed = await Password.toHash(this.get('password'));
+        this.set('password', hashed);
+    }
+});
+
+
+const User : UserModel = mongoose.model<UserDoc, UserModel>('User', userSchema);  // Define  userModel.
+
+// Define custom buildMethode. 
+userSchema.statics.build = (attrs: UserAttrs) => {
+    return new User(attrs);
+}
+
+export { User }; 
