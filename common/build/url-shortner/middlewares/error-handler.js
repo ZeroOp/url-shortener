4dcb0dddfb5768484c4cead201cb362b933843cd
Zeroop <jayaramnaik725@gmail.com>
2026-03-25 16:11:12 +0530

    Updates

--- common/build/url-shortner/middlewares/error-handler.js
+++ common/build/url-shortner/middlewares/error-handler.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.errorHandler = void 0;
-const custom_error_1 = require("../url-shortner/errors/custom-error");
+const errors_1 = require("../errors");
 const errorHandler = (err, req, res, next) => {
-    if (err instanceof custom_error_1.CustomError) {
+    if (err instanceof errors_1.CustomError) {
         return res.status(err.statusCode).send({ errors: err.serializeErrors() });
     }
     console.log(err); // if something went wrong that we are not expecting we will log that error .  
