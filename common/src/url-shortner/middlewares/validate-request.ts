5bf6a9ee2d76e4dd54f64feed61f9982668e717f
Zeroop <jayaramnaik725@gmail.com>
2026-03-25 16:11:29 +0530

    Updates

--- common/src/url-shortner/middlewares/validate-request.ts
+++ common/src/url-shortner/middlewares/validate-request.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { validationResult } from "express-validator";
-import { RequestValidationError } from "../url-shortner/errors/request-validation-error";
+import { RequestValidationError } from "../errors";
 
 export const validateRequest = (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
