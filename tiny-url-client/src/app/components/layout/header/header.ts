fcc416fd16194b25c4b7de20d9d9c82f9b0e21ae
Zeroop <jayaramnaik725@gmail.com>
2026-04-03 01:13:50 +0530

    Updated dynamic routes on header based on user is authenticated or not

--- tiny-url-client/src/app/components/layout/header/header.ts
+++ tiny-url-client/src/app/components/layout/header/header.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../core/services/auth';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, RouterModule],
+  imports: [MatToolbarModule, MatButtonModule, RouterModule, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class HeaderComponent { 
+  // Injecting the auth service to access the login state
+  public authService = inject(AuthService);
+  private router = inject(Router);
+
+  onLogout() {
+    this.authService.logout().subscribe({
+      next: () => {
+        // After successful server-side signout, redirect to home or signin
+        this.router.navigate(['/signin']);
+      },
+      error: (err) => console.error('Logout failed:', err)
+    });
+  }
+  
 }
